7a40d47bcc06d70e05cb3c4bbcffa36f4b7b4157
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-10 11:42:29 +0900

    seed restaurants

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "bootsnap", require: false
 
 # Added
 gem "simple_form", github: "heartcombo/simple_form"
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Destroying all restaurants....'
+Restaurant.destroy_all
+puts "...all restaurants are destroyed. #{Restaurant.count} restaurants left."
+
+puts 'Creating new restaurants...'
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
+puts "...#{Restaurant.count} restaurants have been created."
