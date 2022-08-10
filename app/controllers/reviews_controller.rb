8f74273ea7232a8509e3865ff7a1cf1c962aa303
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-10 12:02:40 +0900

    controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,20 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
